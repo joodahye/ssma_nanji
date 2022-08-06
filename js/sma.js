a83494ae17caade0cc13697838129aca7f5f0496
joodahye <106372964+joodahye@users.noreply.github.com>
2022-08-06 18:22:24 +0900

    Add files via upload

---
+++ js/sma.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+
+    /*메뉴(햄버거클릭)*/
+    $("#head button").click(function(){
+        
+        $("#menu").stop().animate({right: 0}, 1200,function(){
+            if($("#menu").css("right")== "0"){
+                $("#menu button span").html("menu");
+            }else{
+                $("#menu button span").html("close");
+            }
+					});
+					
+					
+          
+						$("#menu button").click(function(){
+						$("#menu").stop().animate({right:-400},1200,function(){
+            if($("#menu").css("right")== "-200"){
+                $("#menu button span").html("close");
+            }else{
+                $("#menu button span").html("menu");
+						}
+					});  
+       
+			});
+	});
+	
+			/*메뉴안의 목록클릭*/
+			$("#navTop li a").click(function(){
+				$("#insideNav").stop().slideToggle();
+			});
+			
+			
+	
+		
+});//끝
+
+
